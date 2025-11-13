2c62fe552a364b39c2b5ed9df524283f3e7e27e6
strc12 <ric@oundleschool.org.uk>
2025-11-13 09:45:52 +0000

    addusers done

--- adduser.php
+++ adduser.php
@@ -1,4 +1,19 @@
 <?php
     print_r($_POST);
     include_once("connection.php");
+    $username="bob";
+    $role=1;
+    $stmt1= $conn->prepare("INSERT INTO tblusers
+    (UserID,Username, Surname, Forename, Password, Year, Balance, Role)
+    VALUES
+    (NULL,:Username, :Surname, :Forename, :Password, :Year, :Balance, :Role)
+    ");
+    $stmt1->bindParam(":Username",$username);
+    $stmt1->bindParam(":Surname",$_POST["surname"]);
+    $stmt1->bindParam(":Forename",$_POST["forename"]);
+    $stmt1->bindParam(":Password",$_POST["password"]);
+    $stmt1->bindParam(":Year",$_POST["year"]);
+    $stmt1->bindParam(":Balance",$_POST["balance"]);
+    $stmt1->bindParam(":Role",$role);
+    $stmt1->execute();
 ?>
