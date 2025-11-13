38029956135530ec0013d958baa32988c8265816
strc12 <ric@oundleschool.org.uk>
2025-11-13 09:11:04 +0000

    users page made

--- users.php
+++ users.php
@@ -8,11 +8,13 @@
     <form>
         Surname: <input type="text" name="surname"><br>
         Forename: <input type="text" name="forename"><br>
-        Password: <input type="text" name="password"><br>
-        Year: <input type="text" name="year"><br>
-        Initial Balance: <input type="text" name="balance"><br>
-        Role: <input type="text" name="surname"><br>
-        Surname: <input type="text" name="surname"><br>
+        Password: <input type="password" name="password"><br>
+        Year: <input type="number" name="year"><br>
+        Initial Balance: <input type="number" name="balance"><br>
+        Role: <br>
+        <input type="radio" name="role" value="pupil" checked>pupil<br>
+        <input type="radio" name="role" value="admin" >Admin<br>
+        <input type="submit" value="Add user"><br>
     </form>
 </body>
 </html>
