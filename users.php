5542042cf2725d92cfe86d577a6fa230b412d1a9
strc12 <ric@oundleschool.org.uk>
2025-11-13 09:06:44 +0000

    for tom

---
+++ users.php
@@ -0,0 +1,18 @@
+<!DOCTYPE HTML>
+<html>
+<head>          
+    <title>Add users</title>
+</head>
+
+<body>
+    <form>
+        Surname: <input type="text" name="surname"><br>
+        Forename: <input type="text" name="forename"><br>
+        Password: <input type="text" name="password"><br>
+        Year: <input type="text" name="year"><br>
+        Initial Balance: <input type="text" name="balance"><br>
+        Role: <input type="text" name="surname"><br>
+        Surname: <input type="text" name="surname"><br>
+    </form>
+</body>
+</html>
