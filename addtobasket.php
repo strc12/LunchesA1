3a4f46e63a13beb1eeb52cba76ed57318667a847
strc12 <ric@oundleschool.org.uk>
2025-12-04 09:41:33 +0000

    add to basket

--- addtobasket.php
+++ addtobasket.php
@@ -1,10 +1,11 @@
 <?php
     session_start();
     print_r($_POST);
+    include_once("connection.php");
     if(!isset($_SESSION["orderinprogress"])){
        
         $dateoforder=date_create()->format('Y-m-d H:i:s');
-        include_once("connection.php");
+        
         $status="Pending";
         $stmt1= $conn->prepare("INSERT INTO tblorder
         (OrderID,Status,UserID,Orderdate)
@@ -17,5 +18,17 @@
         
         $stmt1->execute();
         $_SESSION["orderinprogress"]=1;
-    }
+        $_SESSION["lastorderid"]=$conn->lastInsertId();#stores the last order id created
+        print_r($_SESSION["lastorderid"]);
+    }#add item to basket table
+        $stmt1= $conn->prepare("INSERT INTO tblbasket
+        (OrderID,FoodID,Quantity)
+        VALUES
+        (:orderid,:foodid, :quantity)
+        ");
+        $stmt1->bindParam(":orderid",$_SESSION["lastorderid"]);
+        $stmt1->bindParam(":foodid",$_POST["foodid"]);
+        $stmt1->bindParam(":quantity",$_POST["qty"]);
+        
+        $stmt1->execute();
 ?>
