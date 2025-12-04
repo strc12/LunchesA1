98857da4946dd1062a2740679ae02fd6863ada8d
strc12 <ric@oundleschool.org.uk>
2025-12-04 09:23:14 +0000

    prevent new order every time

--- addtobasket.php
+++ addtobasket.php
@@ -1,17 +1,21 @@
 <?php
     session_start();
     print_r($_POST);
-    $dateoforder=date_create()->format('Y-m-d H:i:s');
-    include_once("connection.php");
-    $status="Pending";
-    $stmt1= $conn->prepare("INSERT INTO tblorder
-    (OrderID,Status,UserID,Orderdate)
-    VALUES
-    (NULL,:status, :userid, :orderdate)
-    ");
-    $stmt1->bindParam(":status",$status);
-    $stmt1->bindParam(":userid",$_SESSION["loggedinuser"]);
-    $stmt1->bindParam(":orderdate", $dateoforder);
-    
-    $stmt1->execute();
+    if(!isset($_SESSION["orderinprogress"])){
+       
+        $dateoforder=date_create()->format('Y-m-d H:i:s');
+        include_once("connection.php");
+        $status="Pending";
+        $stmt1= $conn->prepare("INSERT INTO tblorder
+        (OrderID,Status,UserID,Orderdate)
+        VALUES
+        (NULL,:status, :userid, :orderdate)
+        ");
+        $stmt1->bindParam(":status",$status);
+        $stmt1->bindParam(":userid",$_SESSION["loggedinuser"]);
+        $stmt1->bindParam(":orderdate", $dateoforder);
+        
+        $stmt1->execute();
+        $_SESSION["orderinprogress"]=1;
+    }
 ?>
