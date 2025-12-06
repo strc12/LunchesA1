65a581f81ea36046bb83ab8786dad36e47498bf8
strc12 <ric@oundleschool.org.uk>
2025-12-06 11:48:30 +0000

    check if already in basket

--- addtobasket.php
+++ addtobasket.php
@@ -18,13 +18,28 @@
         $_SESSION["lastorderid"]=$conn->lastInsertId();#stores the last order id created
         print_r($_SESSION["lastorderid"]);
     }#add item to basket table
-        $stmt1= $conn->prepare("INSERT INTO tblbasket
-        (OrderID,FoodID,Quantity)
-        VALUES
-        (:orderid,:foodid, :quantity)
-        ");
+        #check if already in basket, if so update quantity
+        $stmt1=$conn->prepare("SELECT * FROM tblbasket WHERE OrderID=:orderid AND FoodID=:foodid");
         $stmt1->bindParam(":orderid",$_SESSION["lastorderid"]);
         $stmt1->bindParam(":foodid",$_POST["foodid"]);
-        $stmt1->bindParam(":quantity",$_POST["qty"]);
         $stmt1->execute();
+        $count=0;
+        while($row=$stmt1->fetch(PDO::FETCH_ASSOC))
+        {
+            $count=$count+1;
+        }
+        #if not found add to basket
+        if($count==0){
+            $stmt1= $conn->prepare("INSERT INTO tblbasket
+            (OrderID,FoodID,Quantity)
+            VALUES
+            (:orderid,:foodid, :quantity)
+            ");
+            $stmt1->bindParam(":orderid",$_SESSION["lastorderid"]);
+            $stmt1->bindParam(":foodid",$_POST["foodid"]);
+            $stmt1->bindParam(":quantity",$_POST["qty"]);
+            $stmt1->execute();
+        }else{
+            echo("Item already in basket, updating quantity");
+        }
 ?>
